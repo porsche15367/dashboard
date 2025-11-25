5eb7b9bb76ccc7d70403c0957533ef1c19e3a87b
porsche15367 <ahmedgamalor@gmail.com>
2025-11-25 09:28:26 +0200

    chore: Switch API_BASE_URL from local to production endpoint.

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const API_BASE_URL = "https://backend-production-e593.up.railway.app/api";  
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "https://backend-production-e593.up.railway.app/api";
+//const API_BASE_URL = "http://localhost:3000/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
